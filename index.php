06069409735510ba6f9349ab2882ecf319fa6ee3
wpnotifications <wpnotifications@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-27 20:45:37 +0000

    updated button
    git-svn-id: https://plugins.svn.wordpress.org/print-money/trunk@1188717 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Plugin URI: http://dotphoto.com
  * Author: David Ahmad
  * Author URI: http://vbsocial.com
- * Version: 2.0
+ * Version: 2.1
  * Text Domain: Print-Money
  * License: GPL2
  * Copyright 2015 David Ahmad
